95b1550778628f524b1055f623764e05c276234d
demo-user <sdalitaf@gmail.com>
2025-01-22 19:22:33 +0530

    8th commit

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.21"
     }
   }
@@ -22,48 +22,25 @@ provider "aws" {
 
 provider "aws" {
   alias   = "aws_lab"
-  # profile = "default"
   region  = "us-east-2"
 }
 
-/*
-# all the subnets in the aws region
-data "aws_subnets" "all_subnets" {
-	provider = aws.aws_lab
-    filter {
-    name   = "tag:Name"
-    values = ["${var.env}-abc-subnet-1-pub"]
-  }
-}
-*/
-
 # Data source to get EC2 instance details
 data "aws_instance" "target_instance" {
-  provider = aws.aws_lab
-  instance_id = "i-013e6c98aa223a783" # Replace with your instance ID
+  provider    = aws.aws_lab
+  instance_id = "i-00eb66bae41ed8e8e" # Replace with your instance ID
 }
 
-# All attached volumes to the speficed ec2
-data "aws_ebs_volumes" "attached_volumes"{
-  provider = aws.aws_lab
-  filter {
-    name   = "attachment.instance-id"
-    values = [data.aws_instance.target_instance.id]
-  }
+# Output instance details for debugging
+output "instance_details" {
+  value = data.aws_instance.target_instance
 }
 
-# Create snapshots for all attached volumes
+# Iterate over attached volumes
 resource "aws_ebs_snapshot" "volume_snapshots" {
   provider = aws.aws_lab
-  /*
-  for_each = tomap({ 
-    // for idx, volume_id in data.aws_instance.target_instance.ebs_block_device[*].volume_id : 
-    // idx => volume_id
-    for idx, block_device in data.aws_instance.target_instance.ebs_block_device :
-    idx => block_device.volume_id
-  })
-  */
   for_each = toset(data.aws_instance.target_instance.ebs_block_device[*].volume_id)
+
   volume_id = each.value
   tags = {
     Name        = "Snapshot-${each.value}"
@@ -72,17 +49,7 @@ resource "aws_ebs_snapshot" "volume_snapshots" {
   }
 }
 
-# Output all attached volumes
-output "attached_volumes" {
-  value = data.aws_ebs_volumes.attached_volumes.ids
-}
-
-# Output all snapshots 
-output "all_snapshots" {
-  value = aws_ebs_snapshot.volume_snapshots
-}
-
-# Output info about instance 
-output "instance_info" {
-  value = data.aws_instance.target_instance
+# Output all snapshots
+output "snapshots" {
+  value = { for k, v in aws_ebs_snapshot.volume_snapshots : k => v.id }
 }
